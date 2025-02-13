8756b9133f387bc294685301e2f003bd14c0a9ae
dzyy3 <warrendaisym@gmail.com>
2025-02-13 05:36:16 -0500

    map

--- docs/js/spring2025/textStudies/textStudy2.js
+++ docs/js/spring2025/textStudies/textStudy2.js
@@ -27,9 +27,14 @@ function draw() {
     //text, x pos, y pos, font size, density of dots
     textPoints =  myFont.textToPoints( "hello", 25, 150, fontSize, {sampleFactor: 0.1});
 
-//I don't understand anything! I'm struggling so bad rn. I feel like last semester didn't teach me anything
+//I don't understand anything! I'm struggling so bad rn. I feel like last semester didn't teach me anything I have no idea what maps are and I really struggle with everythint that has to do with i++. I feel like a coding fraud T_T 
   for (let i = 0; i < textPoints.length; i++){
-        fill(219, 171, 103, 20)
+        // fill(219, 171, 103, 80)
+
+        //I had to reference the internet for the code below
+        let textGradient = map(pointArray[i].x, 200, 100, 10, 0)
+
+        fill(219, 171, 103, textGradient)
 
     ellipse(textPoints[i].x, textPoints[i].y, 10)
   }
