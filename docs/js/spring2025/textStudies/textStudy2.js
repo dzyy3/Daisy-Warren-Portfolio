1e5eb2963f2d060f6f3d2583517c8ef5840d9abe
dzyy3 <warrendaisym@gmail.com>
2025-02-13 05:20:47 -0500

    transparent

--- docs/js/spring2025/textStudies/textStudy2.js
+++ docs/js/spring2025/textStudies/textStudy2.js
@@ -10,13 +10,7 @@ function preload() {
   myFont = loadFont('../../js/spring2025/textStudies/assets/comicSans.ttf');
 }
 
-function setup() {
-  
-  
-  //text, x pos, y pos, font size, density of dots
-  textPoints = 
-    myFont.textToPoints( "hello", 25, 150, fontSize, {sampleFactor: 0.1});
-  
+function setup() {  
   
   canvas = createCanvas(400, 200);
   canvas.parent('textStudy-container');
@@ -30,8 +24,13 @@ function setup() {
 
 function draw() {
   background(110, 175, 186);
+    //text, x pos, y pos, font size, density of dots
+    textPoints =  myFont.textToPoints( "hello", 25, 150, fontSize, {sampleFactor: 0.1});
 
+//I don't understand anything! I'm struggling so bad rn. I feel like last semester didn't teach me anything
   for (let i = 0; i < textPoints.length; i++){
+        fill(219, 171, 103, 20)
+
     ellipse(textPoints[i].x, textPoints[i].y, 10)
   }
 }
